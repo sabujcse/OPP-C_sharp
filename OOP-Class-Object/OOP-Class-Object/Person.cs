8d278a29517e8bcd30edab82f8da671e2d60c99a
sabuj3cse <sabuj3cse@gmail.com>
2019-05-27 13:38:08 +0600

    object oriented practice in C#

---
+++ OOP-Class-Object/OOP-Class-Object/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Class_Object
+{
+    class Person
+    {
+        //Class Design 
+        //propertity/data/attribute
+        private string firstName;
+        private string middleName;
+        private string lastname;
+
+        //Resposibility/usability/activity
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string MiddleName
+        {
+            get { return middleName; }
+            set {
+                if (value.Length <3)
+                {
+                    middleName = FirstName; // controlling the field use property
+                                            // Field Private . but controling field using property . 
+
+                }
+                else
+                {
+                    middleName = value;
+                }
+                
+            }
+        }
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+        public string GetFullName()
+        {
+            return FirstName + " " + MiddleName + " " + Lastname;
+        }
+    }
+}
---
+++ OOP-Class-Object/OOP-Class-Object/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Class_Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(); //Object Create
+            //propertity initalization using object
+            person.FirstName = "Dipto";  
+            person.MiddleName = "Kumar";
+            person.Lastname = "Saha";
+            Console.WriteLine(person.GetFullName());
+            Console.ReadKey();
+
+        }
+    }
+}
